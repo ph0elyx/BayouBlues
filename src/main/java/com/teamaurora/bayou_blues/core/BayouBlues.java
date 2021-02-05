264deb64d3f2145e674159d03cdeb1834ed54ca0
Epic312 <kaden_ostendorf@yahoo.com>
2021-02-05 12:51:48 -0700

    basic setup

---
+++ src/main/java/com/teamaurora/bayou_blues/core/BayouBlues.java
@@ -0,0 +1,43 @@
+package com.teamaurora.bayou_blues.core;
+
+import com.minecraftabnormals.abnormals_core.core.util.registry.RegistryHelper;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import static com.teamaurora.bayou_blues.core.BayouBlues.MODID;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("bayou_blues")
+public class BayouBlues
+{
+    public static final String MODID = "bayou_blues";
+    public static final RegistryHelper REGISTRY_HELPER = new RegistryHelper(MODID);
+
+    public BayouBlues() {
+        final IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        REGISTRY_HELPER.register(eventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        eventBus.addListener(this::commonSetup);
+        eventBus.addListener(this::clientSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+
+        });
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+
+        });
+    }
+}
