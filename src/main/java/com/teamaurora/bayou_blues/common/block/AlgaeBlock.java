2af4a2d987cec1ebb923d6d81e1c6658a1767653
Epic312 <kaden_ostendorf@yahoo.com>
2021-02-11 13:44:32 -0700

    growable algae

--- src/main/java/com/teamaurora/bayou_blues/common/block/AlgaeBlock.java
+++ src/main/java/com/teamaurora/bayou_blues/common/block/AlgaeBlock.java
@@ -1,9 +1,6 @@
 package com.teamaurora.bayou_blues.common.block;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.BushBlock;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.BoatEntity;
@@ -18,7 +15,9 @@ import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
-public class AlgaeBlock extends BushBlock {
+import java.util.Random;
+
+public class AlgaeBlock extends BushBlock implements IGrowable {
     protected static final VoxelShape ALGAE_AABB = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
 
     public AlgaeBlock(AbstractBlock.Properties builder) {
@@ -36,5 +35,35 @@ public class AlgaeBlock extends BushBlock {
         FluidState fluidstate = worldIn.getFluidState(pos.down());
         FluidState fluidstate1 = worldIn.getFluidState(pos);
         return (fluidstate.getFluid() == Fluids.WATER || state.getMaterial() == Material.ICE) && fluidstate1.getFluid() == Fluids.EMPTY;
+    }
+
+    @Override
+    public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
+        for (BlockPos pos2 : BlockPos.getAllInBoxMutable(pos.add(-2, -2, -2), pos.add(2, 2, 2))) {
+            if (pos.withinDistance(pos2, 2.0F)) {
+                if (worldIn.getBlockState(pos2).isAir() && worldIn.getFluidState(pos2.down()).getFluid() == Fluids.WATER) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld worldIn, Random rand, BlockPos pos, BlockState state) {
+        for (BlockPos pos2 : BlockPos.getAllInBoxMutable(pos.add(-3, -3, -3), pos.add(3, 3, 3))) {
+            if (pos.withinDistance(pos2, 3.0F)) {
+                if (rand.nextFloat() <= 1.0F - pos2.distanceSq(pos)/18.0F) {
+                    if (worldIn.getBlockState(pos2).isAir() && worldIn.getFluidState(pos2.down()).getFluid() == Fluids.WATER) {
+                        worldIn.setBlockState(pos2, this.getDefaultState());
+                    }
+                }
+            }
+        }
     }
 }
