76c8699b14a2afbd806672043ab32eb9d84e31da
Epic312 <kaden_ostendorf@yahoo.com>
2021-02-11 13:55:38 -0700

    made lily flowers growable

--- src/main/java/com/teamaurora/bayou_blues/common/block/LilyFlowerBlock.java
+++ src/main/java/com/teamaurora/bayou_blues/common/block/LilyFlowerBlock.java
@@ -1,5 +1,6 @@
 package com.teamaurora.bayou_blues.common.block;
 
+import com.teamaurora.bayou_blues.core.registry.BayouBluesBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LilyPadBlock;
@@ -8,6 +9,8 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+
+import java.util.Random;
 
 public class LilyFlowerBlock extends LilyPadBlock {
     protected static final VoxelShape LILY_FLOWER_AABB = Block.makeCuboidShape(3.0D, 1.5D, 3.0D, 13.0D, 13.D, 13.0D);
@@ -23,5 +26,28 @@ public class LilyFlowerBlock extends LilyPadBlock {
 
     public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return LILY_PAD_AABB;
+    }
+
+    public static Block getRandomLily(Random rand) {
+        int type = rand.nextInt(8);
+        switch (type) {
+            case 0:
+                return BayouBluesBlocks.LIGHT_GRAY_LILY.get();
+            case 1:
+                return BayouBluesBlocks.WHITE_LILY.get();
+            case 2:
+                return BayouBluesBlocks.CYAN_LILY.get();
+            case 3:
+                return BayouBluesBlocks.BLUE_LILY.get();
+            case 4:
+                return BayouBluesBlocks.LIGHT_BLUE_LILY.get();
+            case 5:
+                return BayouBluesBlocks.MAGENTA_LILY.get();
+            case 6:
+                return BayouBluesBlocks.PINK_LILY.get();
+            case 7:
+                return BayouBluesBlocks.PURPLE_LILY.get();
+        }
+        return null;
     }
 }
--- src/main/java/com/teamaurora/bayou_blues/core/other/BayouBluesEvents.java
+++ src/main/java/com/teamaurora/bayou_blues/core/other/BayouBluesEvents.java
@@ -2,13 +2,19 @@ package com.teamaurora.bayou_blues.core.other;
 
 import com.teamaurora.bayou_blues.common.block.CypressKneeBlock;
 import com.teamaurora.bayou_blues.common.block.DoubleCypressKneeBlock;
+import com.teamaurora.bayou_blues.common.block.LilyFlowerBlock;
 import com.teamaurora.bayou_blues.core.BayouBlues;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorldReader;
+import net.minecraft.world.IWorldWriter;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.player.BonemealEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -23,6 +29,20 @@ public class BayouBluesEvents {
             Block block = state.getBlock();
             if (block instanceof CypressKneeBlock || block instanceof DoubleCypressKneeBlock) {
                 event.getEntity().attackEntityFrom(DamageSource.GENERIC, event.getAmount() * 2);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onBonemealUse(BonemealEvent event) {
+        BlockState state = event.getBlock();
+        BlockPos pos = event.getPos();
+        World world = event.getWorld();
+        if (state.getBlock() == Blocks.LILY_PAD) {
+            Block lily = LilyFlowerBlock.getRandomLily(world.getRandom());
+            if (lily != null) {
+                world.setBlockState(pos, lily.getDefaultState(), 3);
+                event.setResult(Event.Result.ALLOW);
             }
         }
     }
